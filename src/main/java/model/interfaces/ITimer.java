8a629a0a25b095d63dc76cfda41efe0e9ac406d9
Alejandro VS <Alejandrovalenciasandoval@gmail.com>
2026-05-25 22:00:27 -0500

    Se elimino la ruta duplicada

--- src/main/java/model/interfaces/ITimer.java
+++
@@ -1,7 +0,0 @@
-package com.example.sudoku_game.model.interfaces;
-
-public interface ITimer {
-    void addSecond();        // Lógica de incremento
-    void reset();            // Reiniciar estado
-    String getFormattedTime();
-}
