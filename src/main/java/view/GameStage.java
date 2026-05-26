2181ef1d0be6b12e614834a52a236f8b3b0537f5
Maria Alejandra Pizarro Sarria <alejapizasa@gmail.com>
2026-05-25 21:22:19 -0500

    Se inicializa rama Vista y se añaden los componentes JavaFX Home, Game y End

---
+++ src/main/java/view/GameStage.java
@@ -0,0 +1,34 @@
+package com.example.sudoku_game.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import com.example.sudoku_game.controller.GameController;
+public class GameStage extends Stage {
+    public GameStage() throws IOException {
+        Image icon = new Image(getClass().getResourceAsStream("/com/example/Sudoku_game/Imagenes/gameLogo.png"));
+
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/example/Sudoku_game/view/GameView.fxml")
+        );
+
+        Parent root = loader.load();
+        GameController controller = loader.getController();
+        this.setResizable(false);
+        this.setTitle("Sudoku game");
+        this.getIcons().add(icon);
+
+
+        Scene scene = new Scene(root);
+        controller.setupKeyEvents(scene);
+        setScene(scene);
+
+        show();
+    }
+}
